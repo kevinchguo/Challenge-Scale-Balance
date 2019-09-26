d7decb0b06d3b88cc20099acaffe29a1d6804047
Kevin Guo <Kevin@Kevins-Air.home>
2019-09-25 19:38:18 -1000

    not to sure how to organize the else statement for 'not possible'

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 function scaleBalance(strArr) {
+  let scaleArr = changeToNumArr(strArr[0]);
+  let weightsArr = changeToNumArr(strArr[1]);
 
+  function changeToNumArr(givenArr) {
+    let newArr = [];
+    for (let x in givenArr) {
+      if (!isNaN(parseInt(givenArr[x]))) {
+        newArr.push(parseInt(givenArr[x]));
+      }
+    }
+    return newArr;
+  }
+
+  let sortedScale = scaleArr.sort(function(a, b) {
+    return a - b;
+  });
+  let sortedWeights = weightsArr.sort(function(a, b) {
+    return a - b;
+  });
+
+  let scaleDiff = sortedScale[1] - sortedScale[0];
+  let answer = [];
+
+  if (sortedWeights.includes(scaleDiff)) {
+    answer.push(scaleDiff);
+  }
+  for (let i = 0; i < sortedWeights.length; i++) {
+    for (let j = 0; j < sortedWeights.length; j++) {
+      if (sortedWeights[i] + scaleDiff === sortedWeights[j]) {
+        answer.push(sortedWeights[i]);
+        answer.push(sortedWeights[j]);
+      }
+    }
+  }
+  if (answer.length === 1 || answer.length === 2) {
+    return answer.join();
+  } else {
+    return "not possible";
+  }
 }
 
 module.exports = scaleBalance;
--- test/spec.js
+++ test/spec.js
@@ -1,1 +1,19 @@
 // write tests here
+const chai = require("chai");
+const expect = chai.expect;
+const scaleBalance = require("../index.js");
+
+describe("Balance the given array", function() {
+  it("should return a string", function() {
+    var result = scaleBalance(["[5, 9]", "[1, 2, 6, 7]"]);
+    expect(result).to.equal("2,6");
+  });
+  it("should return a string if only 1 weight is used", function() {
+    var result = scaleBalance(["[5, 9]", "[1, 2, 4, 7]"]);
+    expect(result).to.equal("4");
+  });
+  it("should return 'not possible' if there is no solution", function() {
+    var result = scaleBalance(["[5, 9]", "[12, 123, 1234, 12345]"]);
+    expect(result).to.equal("not possible");
+  });
+});
